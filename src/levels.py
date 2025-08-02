a91bed76b61901396bf92ee75d325ea1e31f9248
Tarun BV <152138059+TarunaBV@users.noreply.github.com>
2025-08-02 09:47:47 +0530

    Add files via upload

---
+++ src/levels.py
@@ -0,0 +1,52 @@
+
+# levels.py
+
+levels = [
+    {
+        "name": "Level 1",
+        "word_count": 5,
+        "words": [
+            "code", "loop", "data", "list", "test"
+        ],
+        "enemy_speed_range": (1, 2),
+        "spawn_delay": 2500
+    },
+    {
+        "name": "Level 2",
+        "word_count": 7,
+        "words": [
+            "input", "while", "print", "start", "light", "click", "raise"
+        ],
+        "enemy_speed_range": (2, 3),
+        "spawn_delay": 2200
+    },
+    {
+        "name": "Level 3",
+        "word_count": 10,
+        "words": [
+            "return", "object", "lambda", "global", "random", "window", "button", "define", "script", "output"
+        ],
+        "enemy_speed_range": (2, 4),
+        "spawn_delay": 2000
+    },
+    {
+        "name": "Level 4",
+        "word_count": 12,
+        "words": [
+            "compile", "execute", "element", "package", "pointer", "context", "display", "refresh",
+            "capture", "command", "builder", "sandbox"
+        ],
+        "enemy_speed_range": (3, 4),
+        "spawn_delay": 1800
+    },
+    {
+        "name": "Level 5",
+        "word_count": 15,
+        "words": [
+            "function", "variable", "graphics", "backpack", "terminal", "parallel", "keyboard", "compress",
+            "iterator", "operator", "umbrella", "sandwich", "fireball", "avocado", "debugger"
+        ],
+        "enemy_speed_range": (3, 5),
+        "spawn_delay": 1600
+    }
+]
